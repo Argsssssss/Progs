9aa31e915fb77e6831d395092614a8df30f97ff7
Cryptofun.jr <61570493+Argsssssss@users.noreply.github.com>
2026-03-17 10:11:15 +0700

    Update ULYLYLYLY_create&open_on_while_true.cpp

--- ULYLYLYLY_create&open_on_while_true.cpp
+++ ULYLYLYLY_create&open_on_while_true.cpp
@@ -1,35 +1,22 @@
 #include <iostream>
-
 using namespace std;
 
+#define mem(open_file){int i = 0;while(i < 10){string create = "echo ULYLYLYLYLYLYLYLYLYLYLYLYLYLYLYLYYLYLYYLYLYLLYYLYLYLYLYLY > qwe_", BUFF = open_file;create += to_string(i) + ".txt";open_file += to_string(i) + ".txt";const char* com_create = create.c_str();const char* com_open = open_file.c_str();system(com_create);system(com_open);open_file = BUFF;i++;clear();}}
+
 #if defined(_WIN32) || defined (_WIN64)
+    string open_file = "start qwe_";
     void clear(){
         system ("cls");
     }
-    void open(){
-
-    }
+    void open(){mem(open_file);}
 #elif defined(unix)
+    string open_file = "open qwe_";
     void clear(){
         system ("clear");
     }
-    void open()
-    {   int i = 0;
-        while(true){
-            string create = "echo ULYLYLYLYLYLYLYLYLYLYLYLYLYLYLYLYYLYLYYLYLYLLYYLYLYLYLYLY > qwe_", open = "open qwe_";
-            create += to_string(i) + ".txt";
-            open += to_string(i) + ".txt";
-            const char* com_create = create.c_str();
-            const char* com_open = open.c_str();
-            system(com_create);
-            system(com_open);
-            i++;
-            clear();
-        }
-    }
+    void open(){mem(open_file);}
 #else 
     #error "Platform is not supported"
-
 #endif
 
 int main(){
